dbea08ad088e3f3293168e9bceaf346fd17fbdbc
Arnel Imperial <arneli74@protonmail.com>
2022-09-25 01:46:01 +0300

    🐛 fix(part1/unicafe/src/app.js): solution for part1/1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,28 +5,6 @@ const Title = (props) => {
   return <h1>{title}</h1>;
 };
 
-const Total = (props) => {
-  return props.good + props.neutral + props.bad;
-};
-
-const Average = (props) => {
-  let total = props.good + props.neutral + props.bad;
-  let good = props.good;
-  let bad = props.bad;
-  let calc = good - bad;
-  let average = 0;
-  average = calc / total;
-
-  return(isNaN(average) ? 0 : average)
-};
-
-const Positive = (props) => {
-  let total = props.good + props.neutral + props.bad;
-  let good = props.good;
-  let positive = good / total
-  return(isNaN(positive) ? 0 : positive + "%")
-}
-
 const App = () => {
   const title = "give feedback";
   const [good, setGood] = useState(0);
@@ -42,12 +20,6 @@ const App = () => {
       good {good} <br />
       neutral {neutral} <br />
       bad {bad}
-      <br />
-      all <Total good={good} neutral={neutral} bad={bad} />
-      <br />
-      average <Average good={good} neutral={neutral} bad={bad}/> 
-      <br />
-      positive <Positive good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
