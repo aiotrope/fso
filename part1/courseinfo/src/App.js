f2d7274a25d43ff998032c4836652b912cb31fe4
Arnel Imperial <arneli74@protonmail.com>
2022-09-24 21:32:08 +0300

    ♻️ refactor(part1/courseinfo/src/app.js): fix code to align to the exercise instruction

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -3,11 +3,20 @@ const Header = (props) => {
 };
 
 const Content = (props) => {
+  // trial
+  console.log(props.parts[0].name);
+
+  let part1 = props.parts[0].name;
+  let part2 = props.parts[1].name;
+  let part3 = props.parts[2].name;
+  let exercises1 = props.parts[0].exercises1;
+  let exercises2 = props.parts[0].exercises2;
+  let exercises3 = props.parts[0].exercises3;
   return (
     <>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
     </>
   );
 };
@@ -19,47 +28,38 @@ const Part = (props) => {
   );
 };
 const Total = (props) => {
-  return (
-    <p>
-      Number of exercises{" "}
-      {props.exercises1 + props.exercises2 + props.exercises3}
-    </p>
-  );
+  // trial
+  console.log(props.parts[2].exercises, props.parts[0].exercises);
+
+  let total =
+    props.parts[0].exercises +
+    props.parts[1].exercises +
+    props.parts[2].exercises;
+  return <p>Number of exercises {total}</p>;
 };
 
 const App = () => {
   const course = "Half Stack application development";
   const parts = [
     {
-      name: 'Fundamentals of React',
-      exercises: 10
+      name: "Fundamentals of React",
+      exercises: 10,
     },
     {
-      name: 'Using props to pass data',
-      exercises: 7
+      name: "Using props to pass data",
+      exercises: 7,
     },
     {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={parts[0].name}
-        exercises1={parts[0].exercises}
-        part2={parts[1].name}
-        exercises2={parts[1].exercises}
-        part3={parts[2].name}
-        exercises3={parts[2].exercises}
-      />
-      <Total
-        exercises1={parts[0].exercises}
-        exercises2={parts[1].exercises}
-        exercises3={parts[2].exercises}
-      />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
