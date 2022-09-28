d02fad10e5a62471ebddb7d5278f285948f9ddf6
Arnel Imperial <arneli74@protonmail.com>
2022-09-29 01:44:43 +0300

    🧹 chore(part1/anecdotes/src/app.js): edit file typos

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -27,7 +27,7 @@ const App = () => {
     <div>
       {anecdotes[selected]}
       <br />
-      <button onClick={randomAnecdotes()}>next anectode</button>
+      <button onClick={randomAnecdotes()}>next anecdote</button>
     </div>
   );
 };
