f3e1911dbeaa0a90defe7da60abb87fc052d2460
Arnel Imperial <arneli74@protonmail.com>
2022-10-01 18:02:16 +0300

    ✨ feat(part2/phonebook/src/app.js): solution for part2/phonebook exercise 2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,33 +3,35 @@ import { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
-  
-  
+
   const onSubmit = (event) => {
-    // prevent page reload that can cause the loss of data upon submission
     event.preventDefault();
-    // insert data on the persons array with newName as data entry
-    setPersons((tempPersons) => [...tempPersons, {name: newName}]);
-    // reset form after submission
-    
-    setNewName("")
-    //event.target.reset();
+
+    // get only the values of property name in array of objects
+    const copy = [...persons];
+    const nameArr = copy.map((el) => el.name);
+    // check for match entry
+    const haveMatch = nameArr.includes(newName);
+    const result = haveMatch
+      ? alert(`${newName} is already added to phonebook`)
+      : setPersons((tempPersons) => [...tempPersons, { name: newName }]); // add only if there is no match
+
+    // reset form
+    setNewName("");
+
+    return result;
   };
 
-  /* function that handles the changes of name input 
-  * targeting the changes of name input value based on new entry as object
-  */
   const onChange = (event) => {
-    setNewName(() => (event.target.value));
+    setNewName(() => event.target.value);
   };
-  console.log(newName);
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={onSubmit}>
         <div>
-          name:{" "}
-          <input onChange={onChange} value={newName} type="text" />
+          name: <input onChange={onChange} value={newName} type="text" />
         </div>
         <div>
           <button type="submit">add</button>
