55b090693f7886e59287c9399f5f75f135508c77
Arnel Imperial <arneli74@protonmail.com>
2022-10-07 17:57:37 +0300

    🐛 fix(part2/phonebook/src): edit files

--- part2/phonebook/src/Components/Persons.js
+++ part2/phonebook/src/Components/Persons.js
@@ -1,27 +1,17 @@
 export const Persons = ({ persons, search }) => {
-  const data = persons.map((el, index) => {
-    return (
-      <div key={index}>
-        {el.name} {el.number}
-      </div>
-    );
-  });
-
-  // filter and search array persons againts search prop
-  const filterSearch = persons
-    .filter((person) => {
-      if (person.name.toUpperCase().includes(search.toUpperCase())) {
-        return data;
-      } else {
-        return "";
-      }
-    })
-    .map((el, index) => {
-      return (
-        <div key={index}>
-          {el.name} {el.number}
-        </div>
-      );
-    });
-  return <>{filterSearch}</>;
+  return (
+    <>
+      {persons
+        .filter((person) =>
+          person.name.toUpperCase().includes(search.toUpperCase())
+        )
+        .map((el, index) => {
+          return (
+            <div key={index}>
+              {el.name} {el.number}
+            </div>
+          );
+        })}
+    </>
+  );
 };
