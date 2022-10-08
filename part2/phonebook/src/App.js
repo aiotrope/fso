8ae28a1a2994e853181ac22b7a3dbaac56f3aadc
Arnel Imperial <arneli74@protonmail.com>
2022-10-08 08:10:21 +0300

    ♻️ refactor(part2/phonebook/src/): solution for exercise 2.16

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,54 +1,157 @@
-import { Persons } from "./Components/Persons";
-import { PersonForm } from "./Components/PersonForm";
-import { Filter } from "./Components/Filter";
-import { useEffect, useState } from "react";
-import personsService from "./Components/services/persons";
+import { useState, useEffect } from "react";
+import personsService from "./Components/services/persons"; // services
+
+const Filter = ({ search, setSearch }) => {
+  const onChange = (event) => {
+    let keyword = event.target.value;
+    setSearch(keyword);
+  };
+
+  return (
+    <form>
+      <div>
+        filter shown with:
+        <input type="text" name="filter" value={search} onChange={onChange} />
+      </div>
+    </form>
+  );
+};
+
+const Persons = ({ persons, search }) => {
+  return (
+    <>
+      {persons
+        .filter((person) =>
+          person.name.toUpperCase().includes(search.toUpperCase())
+        )
+        .map((el, index) => {
+          return (
+            <div key={index}>
+              {el.name} {el.number}
+            </div>
+          );
+        })}
+    </>
+  );
+};
+
+const PersonForm = ({
+  persons,
+  setPersons,
+  newName,
+  setNewName,
+  newNumber,
+  setNewNumber,
+}) => {
+  const onChangeName = (event) => {
+    const target = event.target.value;
+    setNewName(target);
+  };
+
+  const onChangeNumber = (event) => {
+    const target = event.target.value;
+    setNewNumber(target);
+  };
+
+  const onSubmit = (event) => {
+    if (event) event.preventDefault();
+    const namesArr = persons.map((e) => e.name);
+    const haveMatch = namesArr.includes(newName);
+
+    const regex2 = /^[\+]?\d\d\d[-\s\.]?\d\d\d\d\d\d$/gm;
+    const regex1 = /^[\+]?[0-9]{2,3}[-\s\.]?[0-9]{2,6}[-\s\.]?[0-9]{6,7}$/im;
+    const regRes1 = regex1.test(newNumber);
+    const regRes2 = regex2.test(newNumber);
+
+    if (newName.length === 0) {
+      alert("You forgot to enter your name!");
+    } else if (haveMatch) {
+      alert(`${newName} is already added to phonebook`);
+    } else if (newNumber.length === 0) {
+      alert("You forgot to enter your phone number!");
+    } else if (!regRes1 && !regRes2) {
+      alert(`${newNumber} is not valid phone number!`);
+    } else {
+      // post new entries on the backend
+      const newEntry = { name: newName, number: newNumber };
+      personsService
+        .create(newEntry)
+        .then((response) => {
+          //console.log(response.data);
+          setPersons(persons.concat(newEntry));
+        })
+        .catch((err) => console.log(err));
+      }
+      
+    setNewName("");
+    setNewNumber("");
+  };
+  console.log(persons);
+  return (
+    <>
+      <form onSubmit={onSubmit} noValidate>
+        <div>
+          name:{" "}
+          <input
+            type="text"
+            name="name"
+            onChange={onChangeName}
+            value={newName || ""}
+            required
+          />
+          <br />
+          number:{" "}
+          <input
+            type="text"
+            name="number"
+            onChange={onChangeNumber}
+            value={newNumber || ""}
+            required
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+    </>
+  );
+};
 
 const App = () => {
   const [persons, setPersons] = useState([]);
-  // function pass as a props to PersonForm
-  // as callback
-  const updatePersonsCollection = (newEntry) => {
-    // call post request from service
-    personsService
-      .create(newEntry)
-      .then((response) => {
-        console.log(response.data)
-        setPersons([...persons, response.data]);
-      })
-      .catch((err) => console.log(err));
-  };
   const [search, setSearch] = useState("");
-
-  /* get the initial persons data from dummy json data
-   */
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   useEffect(() => {
-    // call get request from service
-    personsService.getAll().then((res) => {
-      const init = res.data;
-      //console.log(init);
-      setPersons(init);
-    });
+    // get all name/number entries
+    personsService
+      .getAll()
+      .then((res) => {
+        const init = res.data;
+        console.log(init);
+        setPersons(init);
+      })
+      .catch((e) => console.log(e.message));
   }, []);
 
-  console.log("Added persons: ", persons)
   return (
     <div>
       <h2>Phonebook</h2>
-      {/* pass props as variable to child component
-       * for access from base component App.js
-       */}
-      <Filter personsData={persons} search={search} setSearch={setSearch} />
+
+      <Filter search={search} setSearch={setSearch} />
 
       <h3>Add a new</h3>
       <PersonForm
-        updatePersonsCollection={updatePersonsCollection}
+        newName={newName}
+        newNumber={newNumber}
+        setNewName={setNewName}
+        setNewNumber={setNewNumber}
         persons={persons}
+        setPersons={setPersons}
       />
       <h3>Numbers</h3>
-      {/* access props persons and search
-       */}
+
       <Persons persons={persons} search={search} />
     </div>
   );
